70bd263439aa39f9fcef48671e9badb5e089efda
Yağmur Şümşet <85257688+yagmursumset@users.noreply.github.com>
2022-05-26 15:03:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 
-path = "C:\\Users\\yagmu\\OneDrive\\Masaüstü\\aras.jpeg"
+path = "C:\\Users\\yagmu\\Documents\\aras.jpeg"
 img = cv2.imread(path)
 
 cv2.imshow('image', img)
@@ -10,11 +10,13 @@ cv2.imshow('Green', G)
 cv2.imshow('Red', R)
 
 print(img.shape)
-for x in range(0,1024):
-    for y in range(0,768):
+
+for x in range(0,768):
+    for y in range(0,1024):
         if B[x,y] > 100:
             B[x,y] = 10
 
-merged = cv2.merge([B,G,R])
-cv2.imshow(merged)
+a = cv2.merge([B,G,R])
+cv2.imshow('merged', a)
+
 cv2.waitKey(0)
